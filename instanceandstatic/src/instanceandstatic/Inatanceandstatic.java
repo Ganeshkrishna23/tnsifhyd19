a385af31dc0397d10c342037628e175ec68f62b6
Ganeshkrishna23 <21d41a0436@gmail.com>
2024-05-21 22:00:37 +0530

    Add files via upload

---
+++ instanceandstatic/src/instanceandstatic/Inatanceandstatic.java
@@ -0,0 +1,19 @@
+
+package instanceandstatic;
+
+public class Inatanceandstatic {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int c=8;
+		Instance obj=new Instance();
+		System.out.println(obj.b);
+		System.out.println(obj.a);
+		System.out.println(Static.f);
+		System.out.println(Static.g);
+		System.out.println(c);
+
+	}
+
+}
---
+++ instanceandstatic/src/instanceandstatic/Instance.java
@@ -0,0 +1,8 @@
+
+package instanceandstatic;
+
+public class Instance {
+ int a=6;
+ int b=3;
+
+}
