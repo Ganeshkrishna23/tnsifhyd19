0b935845bbb39bff449ce87b9122a1295dbab8cd
Ganeshkrishna23 <21d41a0436@gmail.com>
2024-05-21 21:59:44 +0530

    Add files via upload

---
+++ importingpackage/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module importingpackage {
+}
---
+++ importingpackage/src/myjava/Typecast.java
@@ -0,0 +1,18 @@
+
+package myjava;
+
+public class Typecast {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=36;
+		short b=(short)a;
+		float c=2.5f;
+		double d=c;
+		System.out.println(b);
+		System.out.println(d);
+
+
+	}
+
+}
